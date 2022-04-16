9fd24792de0f9a8df4d7a7d9196f3bbe6037dff7
DavidLesmes <davidl.lesmesj@ecci.edu.co>
2022-04-15 21:20:01 -0500

    Primeros cambios

---
+++ ejemplo/src/main/java/com/prueba/ejemplo/Models/Cargo.java
@@ -0,0 +1,25 @@
+package com.prueba.ejemplo.Models;
+
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cargo", schema = "public")
+
+@Getter
+@Setter
+
+public class Cargo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idCargo")
+    private int idCargo;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+}
