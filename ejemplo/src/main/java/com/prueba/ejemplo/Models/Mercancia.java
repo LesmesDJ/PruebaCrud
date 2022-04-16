73b66a6693ad80f1c68a7a6004be53cd2132d52a
DavidLesmes <davidl.lesmesj@ecci.edu.co>
2022-04-15 22:34:49 -0500

    Cambios Entidad Mercancia

--- ejemplo/src/main/java/com/prueba/ejemplo/Models/Mercancia.java
+++ ejemplo/src/main/java/com/prueba/ejemplo/Models/Mercancia.java
@@ -1,4 +1,44 @@
 package com.prueba.ejemplo.Models;
 
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.Date;
+
+@Entity
+@Table(name = "mercancia")
+@Getter
+@Setter
+
 public class Mercancia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idMercancia")
+    private int idMercancia;
+
+    @Column(name = "descipcion")
+    private String descipcion;
+
+    @Column(name = "ingreso")
+    private Date ingreso;
+
+    @Column(name = "modificacion")
+    private Date modificacion;
+
+    @ManyToOne
+    @JoinColumn(name = "producto")
+    private Producto producto;
+
+    @ManyToOne
+    @JoinColumn(name = "usuIngreso")
+    private Usuario usuIngreso;
+
+    @ManyToOne
+    @JoinColumn(name = "usuModificacion")
+    private Usuario usuModificacion;
+
+    @Column(name = "cantidad")
+    private String cantidad;
+
 }
